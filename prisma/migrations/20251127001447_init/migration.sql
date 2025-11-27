c352d979bc3d803d8c641a927c652fc0fd26ed9e
pholenk <pholenkadi17@gmail.com>
2025-11-27 07:24:26 +0700

    create new schema for master

---
+++ prisma/migrations/20251127001447_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Master" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "favorites" BOOLEAN NOT NULL
+);
