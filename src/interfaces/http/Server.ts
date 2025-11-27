7db5d4e2a9f3500df137b44912f3da315be4e5cb
pholenk <pholenkadi17@gmail.com>
2025-11-27 12:33:07 +0700

    create pet route

--- src/interfaces/http/Server.ts
+++ src/interfaces/http/Server.ts
@@ -10,6 +10,8 @@ import { createAuthController } from '@/controllers/Auth'
 import { createCategoryController } from '../controllers/Category'
 import { createPrismaMasterRepo } from '@/domain/repositories/MasterRepository'
 import { createMasterController } from '../controllers/Master'
+import { createPrismaPetRepo } from '@/domain/repositories/PetRepository'
+import { createPetController } from '../controllers/Pet'
 
 initEnv()
 
@@ -24,18 +26,21 @@ const jwtService = createJwtService()
 const userRepo = createPrismaUserRepo()
 const categoryRepo = createPrismaCategoryRepo()
 const masterRepo = createPrismaMasterRepo()
+const petRepo = createPrismaPetRepo()
 
-const deps = { jwtService, userRepo, categoryRepo, masterRepo }
+const deps = { jwtService, userRepo, categoryRepo, masterRepo, petRepo }
 
 // Controllers
 const authController = createAuthController(deps)
 const categoryController = createCategoryController(deps)
 const masterController = createMasterController(deps)
+const petController = createPetController(deps)
 
 // Routes
 app.post('/auth/login', authController.login)
 
 app.all('/category{/:id}', categoryController.switcher)
 app.all('/master{/:id}', masterController.switcher)
+app.all('/pet{/:id}', petController.switcher)
 
 app.listen(env.PORT, () => console.info('Server running on port 3000'))
